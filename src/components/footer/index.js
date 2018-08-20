00ec6bc9287e7f4667cd35debd1fcafb893a0eb7
Carlos <cbr_2491@hotmail.com>
2018-08-19 20:35:52 -0600

    footer updated

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {faFacebookSquare, faGithub, faLinkedin} from '@fortawesome/free-brands-svg-icons'
 
 import './style.scss';
 
@@ -29,25 +30,27 @@ const Footer = () => {
                   </li>
                 </ul>
               </div>
-              <div className="col-lg-4 col-md-4 col-sm-12 col-xs-12">
+              <div className="col-lg-4 col-md-4 col-sm-12 col-xs-12 footer-right">
                 <p>
                   Copyright &copy; 2018 by Carlos. All right reserved.
-                      </p>
+                </p>
               </div>
               <div className="col-lg-4 col-md-4 col-sm-12 col-xs-12 footer-nav-container">
                 <ul className="footer-nav-icons">
                   <li>
                     <a href="https://cr.linkedin.com/in/carlos-bonilla-robles-108a8b107">
                       <i className="fa fa-linkedin-square fa-2x" aria-hidden="true"></i>
+                      <FontAwesomeIcon icon={faLinkedin} size='2x'/>
                     </a>
                   </li>
                   <li>
                     <a href="https://github.com/rada2491?tab=repositories">
-                      <i className="fa fa-github fa-2x" aria-hidden="true"></i>
+                    <FontAwesomeIcon icon={faGithub} size='2x'/>
                     </a>
                   </li>
                   <li>
                     <a href="https://www.facebook.com/carlos.bonillarobles">
+                    <FontAwesomeIcon icon={faFacebookSquare} size='2x'/>
                     </a>
                   </li>
                 </ul>
